9a134a038e60026c6058626aaa02a45d7be90b76
CrisTerry <crisdc88@gmail.com>
2020-05-09 01:28:25 -0700

    Add API route workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,52 +13,14 @@ module.exports = function(app){
 
     app.get("/api/workouts", function(req, res){
         // sort('day',-1)
-        Workout.find({}).then(responsedata =>{
-            console.log("api workouts", responsedata.exercises)
-          
-                // res.json(responsedata.exercises)
-            
+        Workout.find({}, {},{sort: {'day':1}}).then(lastworkout =>{
            
+            console.log("api workouts", lastworkout)  
+            res.json(lastworkout)
             
         })
     })
 
-    app.post("/api/workouts", function({body}, res){
-       
-        // body.exercises.day=Date.now();
-        console.log("here at post",body)
-
-        Workout.create(
-            body
-            
-        )
-        .then(workout=>{
-            res.json(workout)
-        })
-        .catch(err=>{
-            res.json(err)
-        })
-    })
-
-    app.put("/api/workouts/:id", function(req, res){
-        
-        req.body.day=Date.now();
-        console.log("here at put", req.params.id)
-        console.log("the body", req.body)
-        
-        Workout.findByIdAndUpdate(req.params.id, {
-            $push:{
-                // day: Date.now(),
-                exercises: req.body
-            }
-        })
-        .then(workout=>{
-            res.json(workout)
-        })
-        .catch(err=>{
-            res.json(err)
-        })
-    })
   
   
 }
